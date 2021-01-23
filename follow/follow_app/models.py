df2cd64d84d81bd19cee1ffc237bee7a0bddbb27
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-01-23 23:02:37 +0900

    フォロー関係を表現

--- follow/follow_app/models.py
+++ follow/follow_app/models.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=20)
+    follow_users = models.ManyToManyField("User", related_name="follower_users")
